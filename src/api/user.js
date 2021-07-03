126ada0077c366742709f553b147c843eff8d30c
huangda <2601464763@qq.com>
2021-07-03 23:14:44 +0800

    模板项目删除和修改文件

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,28 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 export function login(data) {
   return request({
-    url: '/vue-admin-template/user/login',
-    method: 'post',
+    //POST /admin/acl/index/login
+    url: "/admin/acl/index/login",
+    method: "POST",
     data
-  })
+  });
 }
 
 export function getInfo(token) {
+  // GET /admin/acl/index/info?token=eeeeaaabbc123
+  // 此处的params其实是用来接收query传参用的,会自动将对象的属性名作为key,属性值作为value
   return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
+    url: "/admin/acl/index/info",
+    method: "GET",
     params: { token }
-  })
+  });
 }
 
 export function logout() {
+  // POST /admin/acl/index/logout
   return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
-  })
+    url: "/admin/acl/index/logout",
+    method: "POST"
+  });
 }
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,34 +1,34 @@
-import { login, logout, getInfo } from '@/api/user'
-import { getToken, setToken, removeToken } from '@/utils/auth'
-import { resetRouter } from '@/router'
+import { login, logout, getInfo } from "@/api/user";
+import { getToken, setToken, removeToken } from "@/utils/auth";
+import { resetRouter } from "@/router";
 
 const getDefaultState = () => {
   return {
     token: getToken(),
-    name: '',
-    avatar: ''
-  }
-}
+    name: "",
+    avatar: ""
+  };
+};
 
-const state = getDefaultState()
+const state = getDefaultState();
 
 const mutations = {
-  RESET_STATE: (state) => {
-    Object.assign(state, getDefaultState())
+  RESET_STATE: state => {
+    Object.assign(state, getDefaultState());
   },
   SET_TOKEN: (state, token) => {
-    state.token = token
+    state.token = token;
   },
   SET_NAME: (state, name) => {
-    state.name = name
+    state.name = name;
   },
   SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar
+    state.avatar = avatar;
   }
-}
+};
 
 const actions = {
-  // user login
+  /*  // user login
   login({ commit }, userInfo) {
     const { username, password } = userInfo
     return new Promise((resolve, reject) => {
@@ -42,57 +42,77 @@ const actions = {
         reject(error)
       })
     })
+  }, */
+  async login({ commit }, userInfo) {
+    const { username, password } = userInfo;
+
+    try {
+      const response = await login({
+        username: username.trim(),
+        password: password
+      });
+      const { data } = response;
+      // 将请求回来的token存入Vuex的state中(相当于存储于内存中)
+      commit("SET_TOKEN", data.token);
+      // cookie相对localStorage的好处:每次发送请求会自动携带该token
+      setToken(data.token);
+    } catch (error) {
+      reject(error);
+    }
   },
 
   // get user info
   getInfo({ commit, state }) {
     return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
+      getInfo(state.token)
+        .then(response => {
+          const { data } = response;
 
-        if (!data) {
-          return reject('Verification failed, please Login again.')
-        }
+          if (!data) {
+            return reject("Verification failed, please Login again.");
+          }
 
-        const { name, avatar } = data
+          const { name, avatar } = data;
 
-        commit('SET_NAME', name)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
-    })
+          commit("SET_NAME", name);
+          commit("SET_AVATAR", avatar);
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
   },
 
   // user logout
   logout({ commit, state }) {
     return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        removeToken() // must remove  token  first
-        resetRouter()
-        commit('RESET_STATE')
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
+      logout(state.token)
+        .then(() => {
+          removeToken(); // must remove  token  first
+          resetRouter();
+          commit("RESET_STATE");
+          resolve();
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
   },
 
   // remove token
   resetToken({ commit }) {
     return new Promise(resolve => {
-      removeToken() // must remove  token  first
-      commit('RESET_STATE')
-      resolve()
-    })
+      removeToken(); // must remove  token  first
+      commit("RESET_STATE");
+      resolve();
+    });
   }
-}
+};
 
 export default {
   namespaced: true,
   state,
   mutations,
   actions
-}
-
+};
