10a4e1c31c0b2e40ea683833bb53d89faa3be59d
huangda <2601464763@qq.com>
2021-07-04 17:14:08 +0800

    完成TradeMark的静态表格渲染

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,10 +1,40 @@
 <template>
-  <div>TradeMark</div>
+  <div>
+    <el-button style="margin-top: 20px" type="primary" icon="el-icon-plus"
+      >添加</el-button
+    >
+    <el-table border style="width: 100%; margin: 20px 0">
+      <el-table-column label="序号" width="80" align="center">
+      </el-table-column>
+      <el-table-column label="品牌名称"> </el-table-column>
+      <el-table-column label="品牌LOGO"> </el-table-column>
+      <el-table-column label="操作"> </el-table-column>
+    </el-table>
+    <!-- 
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange" -->
+    <el-pagination
+      style="text-align: center"
+      :current-page="page"
+      :page-sizes="[2, 3, 10]"
+      :page-size="limit"
+      layout="prev, pager, next, jumper, -> , sizes, total"
+      :total="total"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 <script>
 export default {
   name: "TradeMark",
+  data() {
+    return {
+      page: 1,
+      limit: 3,
+      total: 15,
+    };
+  },
 };
 </script>
 
